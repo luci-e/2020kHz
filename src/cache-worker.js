e636b87685b37329f1452e1232d76b727c77c2e7
Emiliano Luci <luciemiliano@gmail.com>
2021-07-27 13:26:17 +0200

    cleanup

--- src/cache-worker.js
+++ src/cache-worker.js
@@ -27,80 +27,91 @@
 
 var cacheName = 'theCache';
 
+var urlsToCache = [
+    '/modules/misc/ID3.js',
+    '/modules/misc/PapaParse-5.0.2/papaparse.min.js',
+    '/modules/misc/ScrollableList.js',
+    '/pageroot/body/playerManager/songlist/SongListContainer.js',
+    '/pageroot/body/playerManager/songlist/Song.js',
+    '/pageroot/body/playerManager/PlayerManager.js',
+    '/pageroot/body/playerManager/player/Player.js',
+    './pageroot/arrow-right.png',
+    './pageroot/back.png',
+    './pageroot/curve.png',
+    './pageroot/dice.png',
+    './pageroot/folder.png',
+    './pageroot/next.png',
+    './pageroot/shuffle.png',
+    './index.html'
+]
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          '/modules/misc/ID3.js',
-          '/modules/misc/PapaParse-5.0.2/papaparse.min.js',
-          '/modules/misc/ScrollableList.js',
-          '/pageroot/body/playerManager/songlist/SongListContainer.js',
-          '/pageroot/body/playerManager/songlist/Song.js',
-          '/pageroot/body/playerManager/PlayerManager.js',
-          '/pageroot/body/playerManager/player/Player.js',
-          '/index.html'
-        ]
-      );
-    })
-  );
+    console.log('Hello from the service worker');
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            try {
+                return cache.addAll(urlsToCache);
+            } catch (e) {
+                console.log(e);
+            }
+        })
+    );
 });
 
 
 self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
+    console.log('Handling fetch event for', event.request.url);
 
-  event.respondWith(
-    caches.open(cacheName).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        if (response) {
-          // If there is an entry in the cache for event.request, then response will be defined
-          // and we can just return it. Note that in this example, only font resources are cached.
-          console.log(' Found response in cache:', response);
+    event.respondWith(
+        caches.open(cacheName).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                if (response) {
+                    // If there is an entry in the cache for event.request, then response will be defined
+                    // and we can just return it. Note that in this example, only font resources are cached.
+                    console.log(' Found response in cache:', response);
 
-          return response;
-        }
+                    return response;
+                }
 
-        // Otherwise, if there is no entry in the cache for event.request, response will be
-        // undefined, and we need to fetch() the resource.
-        console.log(' No response for %s found in cache. About to fetch ' +
-          'from network...', event.request.url);
+                // Otherwise, if there is no entry in the cache for event.request, response will be
+                // undefined, and we need to fetch() the resource.
+                console.log(' No response for %s found in cache. About to fetch ' +
+                    'from network...', event.request.url);
 
-        // We call .clone() on the request since we might use it in a call to cache.put() later on.
-        // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
-        // (see https://fetch.spec.whatwg.org/#dom-request-clone)
-        return fetch(event.request.clone()).then(function(response) {
-          console.log('  Response for %s from network is: %O',
-            event.request.url, response);
+                // We call .clone() on the request since we might use it in a call to cache.put() later on.
+                // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
+                // (see https://fetch.spec.whatwg.org/#dom-request-clone)
+                return fetch(event.request.clone()).then(function(response) {
+                    console.log('  Response for %s from network is: %O',
+                        event.request.url, response);
 
-          if (response.status < 400) {
-            // This avoids caching responses that we know are errors (i.e. HTTP status code of 4xx or 5xx).
-            // Note that for opaque filtered responses (https://fetch.spec.whatwg.org/#concept-filtered-response-opaque)
-            // we can't access to the response headers, so this check will always fail and the font won't be cached.
-            // All of the Google Web Fonts are served off of a domain that supports CORS, so that isn't an issue here.
-            // It is something to keep in mind if you're attempting to cache other resources from a cross-origin
-            // domain that doesn't support CORS, though!
-            // We call .clone() on the response to save a copy of it to the cache. By doing so, we get to keep
-            // the original response object which we will return back to the controlled page.
-            // (see https://fetch.spec.whatwg.org/#dom-response-clone)
-            console.log('  Caching the response to', event.request.url);
-            cache.put(event.request, response.clone());
-          } else {
-            console.log('  Not caching the response to', event.request.url);
-          }
+                    if (response.status < 400) {
+                        // This avoids caching responses that we know are errors (i.e. HTTP status code of 4xx or 5xx).
+                        // Note that for opaque filtered responses (https://fetch.spec.whatwg.org/#concept-filtered-response-opaque)
+                        // we can't access to the response headers, so this check will always fail and the font won't be cached.
+                        // All of the Google Web Fonts are served off of a domain that supports CORS, so that isn't an issue here.
+                        // It is something to keep in mind if you're attempting to cache other resources from a cross-origin
+                        // domain that doesn't support CORS, though!
+                        // We call .clone() on the response to save a copy of it to the cache. By doing so, we get to keep
+                        // the original response object which we will return back to the controlled page.
+                        // (see https://fetch.spec.whatwg.org/#dom-response-clone)
+                        console.log('  Caching the response to', event.request.url);
+                        cache.put(event.request, response.clone());
+                    } else {
+                        console.log('  Not caching the response to', event.request.url);
+                    }
 
-          // Return the original response object, which will be used to fulfill the resource request.
-          return response;
-        });
-      }).catch(function(error) {
-        // This catch() will handle exceptions that arise from the match() or fetch() operations.
-        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-        // It will return a normal response object that has the appropriate error code set.
-        console.error('  Error in fetch handler:', error);
+                    // Return the original response object, which will be used to fulfill the resource request.
+                    return response;
+                });
+            }).catch(function(error) {
+                // This catch() will handle exceptions that arise from the match() or fetch() operations.
+                // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+                // It will return a normal response object that has the appropriate error code set.
+                console.error('  Error in fetch handler:', error);
 
-        throw error;
-      });
-    })
-  );
+                throw error;
+            });
+        })
+    );
 });
