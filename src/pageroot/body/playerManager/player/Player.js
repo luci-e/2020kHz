44d197047ff39f500c19fc4b089cc43cc03aea60
Emiliano Luci <luciemiliano@gmail.com>
2021-08-04 14:27:34 +0200

    turntable complete

--- src/pageroot/body/playerManager/player/Player.js
+++ src/pageroot/body/playerManager/player/Player.js
@@ -10,6 +10,9 @@ class Player extends HTMLDivElement {
     playingSongTitle;
 
     turntable;
+    mouseDown = false;
+    lastRadians;
+    currentRadians = 0;
 
     constructor() {
         super();
@@ -73,6 +76,16 @@ class Player extends HTMLDivElement {
                 }
             );
 
+            this.turntable.addEventListener('mousedown', (event) => {
+                this.mouseDown = true;
+                this.lastRadians = this.getMouseRadians(event);
+                $(this.turntable).css('transition', 'transform 0s linear');
+             });
+            document.addEventListener('mouseup', () => {
+                this.mouseDown = false;
+                $(this.turntable).css('transition', 'transform .2s linear');
+             });
+            document.addEventListener('mousemove', this.mouse.bind(this));
         })
 
     }
@@ -111,10 +124,43 @@ class Player extends HTMLDivElement {
         }
     }
 
-    async updateTurntable(event){
-        let rotation = 2*Math.PI * event.target.currentTime / event.target.duration;
+    async updateTurntable(event) {
+        let rotation = 2 * Math.PI * event.target.currentTime / event.target.duration;
         this.turntable.style.transform = `rotate(${rotation}rad)`;
     }
+
+    getMouseRadians(event){
+        let img = $(this.turntable);
+        let offset = img.offset();
+        let center_x = (offset.left) + (img.width() / 2);
+        let center_y = (offset.top) + (img.height() / 2);
+        let mouse_x = event.pageX;
+        let mouse_y = event.pageY;
+
+        let radians = Math.atan2(center_y - mouse_y, mouse_x - center_x);
+
+        radians = radians < 0 ? radians + 2*Math.PI : radians;
+
+        return radians;
+    }
+
+    async mouse(event) {
+        if (this.mouseDown) {
+            let img = $(this.turntable);
+            let radians = this.getMouseRadians(event);
+
+            this.currentRadians = this.currentRadians - (radians - this.lastRadians) + (2*Math.PI);
+            this.currentRadians %= (2*Math.PI);
+            this.lastRadians = radians;
+
+            img.css('transform', `rotate(${this.currentRadians}rad)`);
+
+            let newTime = this.currentRadians * this.playingSong.duration / (2*Math.PI);
+            this.playingSong.currentTime = newTime;
+
+        }
+    }
+
 }
 
 customElements.define('player-panel', Player, { extends: 'div' });
